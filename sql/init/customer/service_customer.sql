b10c3e690fb9c279586c4717d20fa2f306a8421e
tranhanh18042 <hanhtran18042@gmial.com>
2023-01-14 18:52:15 +0700

    fix datatype varchar for column last_name

--- sql/init/customer/service_customer.sql
+++ sql/init/customer/service_customer.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `customer` (
     `username` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `first_name` VARCHAR(255) NOT NULL,
-    `last_name` INT NOT NULL,
+    `last_name` VARCHAR(255) NOT NULL,
     `address` VARCHAR(255) NOT NULL,
     `phone_number` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL
