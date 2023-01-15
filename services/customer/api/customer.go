68fe41cc3a57ee9cbec09d1e8733b4e1c22fa84e
tranhanh18042 <hanhtran18042@gmial.com>
2023-01-15 08:51:48 +0700

    update API customer

--- services/customer/api/customer.go
+++ services/customer/api/customer.go
@@ -7,33 +7,33 @@ import (
 )
 
 type Customer struct {
-	Status      int    `json:"Status"`
-	Username    string `json:"Username"`
-	Password    string `json:"Password"`
-	FirstName   string `json:"FirstName"`
-	LastName    string `json:"LastName"`
-	Address     string `json:"Address"`
-	PhoneNumber string `json:"PhoneNumber"`
-	Email       string `json:"Email"`
+	Status      int    `json:"status" db:"Status"`
+	Username    string `json:"username" db:"username"`
+	Password    string `json:"password" db:"password"`
+	FirstName   string `json:"first_name" db:"first_name"`
+	LastName    string `json:"last_name" db:"last_name"`
+	Address     string `json:"address" db:"address"`
+	PhoneNumber string `json:"phone_number" db:"phone_number"`
+	Email       string `json:"email" db:"email"`
 }
 
 type CustomerDetail struct {
-	Id          string `json:"Id"`
-	Status      string `json:"Status"`
-	Username    string `json:"Username"`
-	Password    string `json:"Password"`
-	FirstName   string `json:"FirstName"`
-	LastName    string `json:"LastName"`
-	Address     string `json:"Address"`
-	PhoneNumber string `json:"PhoneNumber"`
-	Email       string `json:"Email"`
+	Id          int    `json:"id" db:"id"`
+	Status      int    `json:"status" db:"status"`
+	Username    string `json:"username" db:"username"`
+	Password    string `json:"password" db:"password"`
+	FirstName   string `json:"first_name" db:"first_name"`
+	LastName    string `json:"last_name" db:"last_name"`
+	Address     string `json:"address" db:"address"`
+	PhoneNumber string `json:"phone_number" db:"phone_number"`
+	Email       string `json:"email" db:"email"`
 }
 
 func CreateCustomer() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var Customer Customer
 		if err := ctx.ShouldBindJSON(&Customer); err == nil {
-			_, err := customerDB.Exec("insert into customer(status, username, password, first_name, last_name, address, phone_number, email) values(?,?,?,?,?,?,?,?)", Customer.Status, Customer.Username, Customer.Password, Customer.FirstName, Customer.LastName, Customer.Address, Customer.PhoneNumber, Customer.Email)
+			_, err := customerDB.Exec("INSERT INTO customer(status, username, password, first_name, last_name, address, phone_number, email) VALUES(?,?,?,?,?,?,?,?)", Customer.Status, Customer.Username, Customer.Password, Customer.FirstName, Customer.LastName, Customer.Address, Customer.PhoneNumber, Customer.Email)
 			if err != nil {
 				ctx.JSON(500, gin.H{
 					"message": err.Error(),
@@ -50,8 +50,8 @@ func CreateCustomer() gin.HandlerFunc {
 func GetCustomerByID() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var CustomerDetail CustomerDetail
-		row := customerDB.QueryRow("select * from customer where id = " + ctx.Param("id"))
-		if err := row.Scan(&CustomerDetail, &CustomerDetail.Status, &CustomerDetail.Username, &CustomerDetail.Password, &CustomerDetail.FirstName, &CustomerDetail.LastName, &CustomerDetail.Address, &CustomerDetail.PhoneNumber, &CustomerDetail.Email); err == nil {
+		row := customerDB.QueryRow("SELECT id, status, username, password, first_name, last_name, address, phone_number, email from customer WHERE id = " + ctx.Param("id"))
+		if err := row.Scan(&CustomerDetail.Id, &CustomerDetail.Status, &CustomerDetail.Username, &CustomerDetail.Password, &CustomerDetail.FirstName, &CustomerDetail.LastName, &CustomerDetail.Address, &CustomerDetail.PhoneNumber, CustomerDetail.Email); err == nil {
 			ctx.JSON(http.StatusUnprocessableEntity, gin.H{
 				"message": err.Error(),
 			})
@@ -63,32 +63,33 @@ func GetCustomerByID() gin.HandlerFunc {
 func GetListCustomer() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var customerDetail []CustomerDetail
-		rows, err := customerDB.Query("select * from customer")
+		rows, err := customerDB.Query("SELECT * FROM customer")
 		if err != nil {
 			panic(err)
 		}
 		for rows.Next() {
 			var singleCustomerDetail CustomerDetail
-			if err := rows.Scan(&singleCustomerDetail.Id, &singleCustomerDetail.Status, &singleCustomerDetail.Username, &singleCustomerDetail.Password, &singleCustomerDetail.FirstName, &singleCustomerDetail.LastName, &singleCustomerDetail.Address, &singleCustomerDetail.PhoneNumber, &singleCustomerDetail.Email); err == nil {
+			if err := rows.Scan(&singleCustomerDetail.Id, &singleCustomerDetail.Status, &singleCustomerDetail.Username, &singleCustomerDetail.Password, &singleCustomerDetail.FirstName, &singleCustomerDetail.LastName, &singleCustomerDetail.Address, &singleCustomerDetail.PhoneNumber, singleCustomerDetail.Email); err == nil {
 				ctx.JSON(http.StatusUnprocessableEntity, gin.H{
-					"message": err.Error(),
+					"message": err,
 				})
 				return
 			}
 			customerDetail = append(customerDetail, singleCustomerDetail)
 		}
+		ctx.JSON(http.StatusOK, customerDetail)
 	}
 }
 func UpdateCustomer() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		var CustomerDetail CustomerDetail
-		if err := ctx.ShouldBindJSON(&CustomerDetail); err == nil {
-			update, err := customerDB.Prepare("update customer set status=?, username=?,password=? ,first_name=?, last_name=?, address=?, phone_number=?, email=? where id=" + ctx.Param("id"))
+		var CustomerUpdate Customer
+		if err := ctx.ShouldBindJSON(&CustomerUpdate); err == nil {
+			update, err := customerDB.Prepare("UPDATE customer SET status=?, username=?,password=? ,first_name=?, last_name=?, address=?, phone_number=?, email=? WHERE id=" + ctx.Param("id"))
 			if err != nil {
 				panic(err)
 			}
-			update.Exec(CustomerDetail.Status, CustomerDetail.Username, CustomerDetail.Password, CustomerDetail.FirstName, CustomerDetail.LastName, CustomerDetail.Address, CustomerDetail.PhoneNumber, CustomerDetail.Email)
-			ctx.JSON(200, CustomerDetail)
+			update.Exec(CustomerUpdate.Status, CustomerUpdate.Username, CustomerUpdate.Password, CustomerUpdate.FirstName, CustomerUpdate.LastName, CustomerUpdate.Address, CustomerUpdate.PhoneNumber, CustomerUpdate.Email)
+			ctx.JSON(200, CustomerUpdate)
 		} else {
 			ctx.JSON(500, gin.H{
 				"message": "error",
